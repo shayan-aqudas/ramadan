aaa8a419185d7419f1718a804f6e3c6d82d80b62
Shayan Aqudas <69024693+shayan-aqudas@users.noreply.github.com>
2024-03-03 00:24:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ setTimeout(()=>{
 
 
 // Heading Date Section
-let days = ["Monday","Tuesday","Wednesday","Thirsday","Friday","Saturday","Sunday"];
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thirsday","Friday","Saturday"];
 let months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 let date = new Date();
